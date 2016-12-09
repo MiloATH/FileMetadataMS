e9aaa20f08bd94de04988dcc1af3c4c5a09ac88c
Milo Hartsoe <milo.red.duck@gmail.com>
2016-12-09 19:42:59 +0000

    Adds to index and response

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var path = require('path');
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' });
+var multer = require('multer');
+var upload = multer({
+    dest: 'uploads/'
+});
 var express = require('express');
-var app  = express();
+var app = express();
 var PORT = process.env.PORT || 3500;
 
 //home
@@ -16,10 +18,23 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/upload', upload.single('file'), function(req,res){
-    
+app.post('/upload', upload.single('file'), function(req, res) {
+    var file = req.file;
+    if (file) {
+        var ret = {
+            size: file.size,
+            encoding: file.encoding
+        };
+        //console.log(file.size);
+        res.json(ret);
+    }
+    else {
+        res.json({
+            error: 'File not readable'
+        });
+    }
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function() {
     console.log('File metadata ms listening on ', PORT);
 });
